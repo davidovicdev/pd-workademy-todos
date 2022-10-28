f6a34d3f38c0b48916103a0f883aa70de7b65086
Matija Davidovic <davidovicdev@gmail.com>
2022-10-28 10:00:54 +0200

    Added Controllers

---
+++ src/Web/PD.Workademy.Todo.Web/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PD.Workademy.Todo.Web.Controllers
+{
+    [ApiController]
+    public class CategoryController : ApiBaseController
+    {
+        private readonly List<string> _categories = new() { "Easy", "Hard", "Medium", "The hardest", "The easiest"};
+        [HttpGet("/api/[controller]")]
+        public async Task<ActionResult> GetAllCategoriesAsync()
+        {
+            return Ok(_categories);
+        }
+        [HttpGet("/api/[controller]/{id}")]
+        public async Task<ActionResult> GetCategoryAsync(Guid id)
+        {
+            return id != Guid.Empty ? Ok(_categories[0]) : Ok("It's not guid") ;
+        }
+
+    }
+}
