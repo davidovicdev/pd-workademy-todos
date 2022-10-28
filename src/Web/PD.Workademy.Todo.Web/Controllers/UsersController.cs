3a77d73551d0bf52e15c150a8a9765574bfe0372
Matija Davidovic <davidovicdev@gmail.com>
2022-10-28 16:48:43 +0200

    Every controller is finished

--- src/Web/PD.Workademy.Todo.Web/Controllers/UsersController.cs
+++ src/Web/PD.Workademy.Todo.Web/Controllers/UsersController.cs
@@ -1,7 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PD.Workademy.Todo.Web.ApiModels;
 
 namespace PD.Workademy.Todo.Web.Controllers
 {
-    [ApiController]
-    public class UsersController : ApiBaseController { }
+    public class UsersController : ApiBaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetAllUsersAsync()
+        {
+            List<UserDTO> existingUsers =
+                new()
+                {
+                    new UserDTO(
+                        new Guid("97df5413-9454-4a56-abbd-c918373bbe3c"),
+                        "Matija",
+                        "Davidovic"
+                    ),
+                    new UserDTO(
+                        new Guid("d209a3b5-9fb9-4e44-8b8d-f5f6fc502732"),
+                        "Nikola",
+                        "Djokic"
+                    ),
+                    new UserDTO(
+                        new Guid("146c074a-039b-46ab-8ac4-a7fb4a80ec8e"),
+                        "Aleksandar",
+                        "Vidakovic"
+                    ),
+                };
+            return Ok(existingUsers);
+        }
+    }
 }
