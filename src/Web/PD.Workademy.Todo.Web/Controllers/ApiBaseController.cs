3c4f0a8c8287b6de4d34e3b6679efd3c2c19cf47
Matija Davidovic <davidovicdev@gmail.com>
2022-11-03 10:40:29 +0100

    Git change

--- src/Web/PD.Workademy.Todo.Web/Controllers/ApiBaseController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using PD.Workademy.Todo.Web.ApiModels;
-
-namespace PD.Workademy.Todo.Web.Controllers
-{
-    [Route("[controller]")]
-    [ApiController]
-    public class ApiBaseController : ControllerBase { }
-}
