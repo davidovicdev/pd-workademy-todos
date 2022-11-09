7dccb329fe6ddf5c9d55832f141c53c354ac0209
Matija Davidovic <davidovicdev@gmail.com>
2022-11-09 13:16:30 +0100

    InfrastructureStartup finished

---
+++ src/Infrastructure/PD.Workademy.Todo.Infrastructure/InfrastructureStartup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PD.Workademy.Todo.Domain.SharedKernel.Interfaces.Repositories;
+using PD.Workademy.Todo.Infrastructure.Persistance;
+using PD.Workademy.Todo.Infrastructure.Persistance.Repositories;
+
+namespace PD.Workademy.Todo.Infrastructure
+{
+    public class InfrastructureStartup
+    {
+        public InfrastructureStartup(IConfigurationRoot configurationRoot)
+        {
+            Configuration = configurationRoot;
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureService(IServiceCollection services)
+        {
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<ITodoItemRepository, TodoItemRepository>();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("Todos"));
+            });
+        }
+    }
+}
--- src/Web/PD.Workademy.Todo.Web/Program.cs
+++ src/Web/PD.Workademy.Todo.Web/Program.cs
@@ -1,11 +1,14 @@
 using PD.Workademy.Todo.Application;
+using PD.Workademy.Todo.Infrastructure;
 using Startup = PD.Workademy.Todo.Web.Startup;
 
 var builder = WebApplication.CreateBuilder(args);
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
-var applicationStartup = new ApplicationStartup(builder.Configuration);
+ApplicationStartup applicationStartup = new(builder.Configuration);
 applicationStartup.ConfigureServices(builder.Services);
+InfrastructureStartup infrastructureStartup = new(builder.Configuration);
+infrastructureStartup.ConfigureService(builder.Services);
 var app = builder.Build();
 startup.Configure(app);
 if (app.Environment.IsDevelopment())
@@ -13,7 +16,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.Run();
